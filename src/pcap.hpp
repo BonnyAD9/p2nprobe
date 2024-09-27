f7171b993b20a8b0bbfc7e8315bea82831692f3e
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-09-27 18:49:17 +0200

    Document pcap

--- src/pcap.hpp
+++ src/pcap.hpp
@@ -2,8 +2,11 @@
 
 namespace p2np::pcap {
 
+/// @brief Get last pcap error message.
+/// @return Last pcap error message.
 const char *strerror();
 
+/// @brief Initialize the pcap library.
 void init();
 
 } // namespace p2np::pcap
